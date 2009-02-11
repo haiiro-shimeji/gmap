c37487289c87479c25764fe06ef2d21e6359ab75
Brandon Bergren <bdragon@rtk0.net>
2009-02-11 20:21:41 +0000

    JSLint run. == -> === changes. Remove unused variable. Dot notation.

--- js/gmap_shapes.js
+++ js/gmap_shapes.js
@@ -1,11 +1,14 @@
 /* $Id$ */
 
 /**
+ * @file
  * GMap Shapes
  * GMap API version / Base case
  */
 
-Drupal.gmap.addHandler('gmap', function(elem) {
+/*global $, Drupal, GEvent, GLatLng, GPolygon, GPolyline */
+
+Drupal.gmap.addHandler('gmap', function (elem) {
   var obj = this;
 /*
   obj.bind('init',function() {
@@ -14,27 +17,27 @@ Drupal.gmap.addHandler('gmap', function(elem) {
     }
   });
 */
-  obj.bind('prepareshape',function(shape) {
-    var i, pa, pp, cargs, style;
+  obj.bind('prepareshape', function (shape) {
+    var pa, cargs, style;
     //var m = new GMarker(new GLatLng(marker.latitude,marker.longitude),marker.opts);
     pa = []; // point array (array of GLatLng-objects)
     var fillstyle = true;
-    if (shape.type == 'circle') {
-      pa = obj.poly.calcPolyPoints(new GLatLng(shape.center[0],shape.center[1]), shape.radius * 1000, shape.numpoints);
+    if (shape.type === 'circle') {
+      pa = obj.poly.calcPolyPoints(new GLatLng(shape.center[0], shape.center[1]), shape.radius * 1000, shape.numpoints);
     }
-    else if (shape.type == 'rpolygon') {
+    else if (shape.type === 'rpolygon') {
       shape.center = new GLatLng(shape.center[0], shape.center[1]);
       shape.point2 = new GLatLng(shape.point2[0], shape.point2[1]);
       var radius = shape.center.distanceFrom(shape.point2);
       pa = obj.poly.calcPolyPoints(shape.center, radius, shape.numpoints);
     }
-    else if (shape.type == 'polygon') {
-      $.each(shape.points, function(i, n) {
+    else if (shape.type === 'polygon') {
+      $.each(shape.points, function (i, n) {
         pa.push(new GLatLng(n[0], n[1]));
       });
     }
-    else if (shape.type == 'line') {
-      $.each(shape.points, function(i, n) {
+    else if (shape.type === 'line') {
+      $.each(shape.points, function (i, n) {
         pa.push(new GLatLng(n[0], n[1]));
       });
       fillstyle = false;
@@ -43,13 +46,13 @@ Drupal.gmap.addHandler('gmap', function(elem) {
 
     // Style normalization
     if (fillstyle) {
-      style = obj.vars.styles['poly_default'].slice();
+      style = obj.vars.styles.poly_default.slice();
     }
     else {
-      style = obj.vars.styles['line_default'].slice();
+      style = obj.vars.styles.line_default.slice();
     }
     if (shape.style) {
-      if (typeof shape.style == 'string') {
+      if (typeof shape.style === 'string') {
         if (obj.vars.styles[shape.style]) {
           style = obj.vars.styles[shape.style].slice();
         }
@@ -66,15 +69,15 @@ Drupal.gmap.addHandler('gmap', function(elem) {
       style[4] = style[4] / 100;
     }
 
-    $.each(style, function(i,n){
+    $.each(style, function (i, n) {
       cargs.push(n);
     });
-    var Pg = function(args) {
-      GPolygon.apply(this,args);
+    var Pg = function (args) {
+      GPolygon.apply(this, args);
     };
     Pg.prototype = new GPolygon();
-    var Pl = function(args) {
-      GPolyline.apply(this,args);
+    var Pl = function (args) {
+      GPolyline.apply(this, args);
     };
     Pl.prototype = new GPolyline();
     switch (shape.type) {
@@ -89,7 +92,7 @@ Drupal.gmap.addHandler('gmap', function(elem) {
     }
   });
 
-  obj.bind('addshape', function(shape) {
+  obj.bind('addshape', function (shape) {
     if (!obj.vars.shapes) {
       obj.vars.shapes = [];
     }
@@ -97,19 +100,19 @@ Drupal.gmap.addHandler('gmap', function(elem) {
     obj.map.addOverlay(shape.shape);
 
     if (obj.vars.behavior.clickableshapes) {
-      GEvent.addListener(shape.shape, 'click', function() {
+      GEvent.addListener(shape.shape, 'click', function () {
         obj.change('clickshape', -1, shape);
       });
     }
   });
 
-  obj.bind('delshape',function(shape) {
+  obj.bind('delshape', function (shape) {
     obj.map.removeOverlay(shape.shape);
   });
 
-  obj.bind('clearshapes',function() {
+  obj.bind('clearshapes', function () {
     if (obj.vars.shapes) {
-      $.each(obj.vars.shapes, function(i,n) {
+      $.each(obj.vars.shapes, function (i, n) {
         obj.change('delshape', -1, n);
       });
     }
