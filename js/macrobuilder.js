143426c2b3cd68f0213103423c0c61537165aad5
Brandon Bergren <bdragon@rtk0.net>
2009-02-11 19:59:45 +0000

    JSLint run.

--- js/macrobuilder.js
+++ js/macrobuilder.js
@@ -1,17 +1,21 @@
 /* $Id$ */
 
-////////////////////////////////////////
-//           Map ID widget            //
-////////////////////////////////////////
-Drupal.gmap.addHandler('mapid', function(elem) {
+/**
+ * @file
+ * Map ID widget for macro form.
+ */
+
+/*global $, Drupal */
+
+Drupal.gmap.addHandler('mapid', function (elem) {
   var obj = this;
   // Respond to incoming map id changes.
-  var binding = obj.bind("idchange",function(){
+  var binding = obj.bind("idchange", function () {
     elem.value = obj.vars.macro_mapid;
   });
   // Send out outgoing map id changes.
-  $(elem).change(function() {
+  $(elem).change(function () {
     obj.vars.macro_mapid = elem.value;
-    obj.change("idchange",binding);
+    obj.change("idchange", binding);
   });
 });
