f5506d3480a65048ebe17037fa365bbdf92300ea
ikeda <ikeda@digitalearth.co.jp>
2012-06-07 17:06:29 +0900

    import

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/gmap/"
+http_images_path = "/gmap/images"
+http_fonts_path = "/gmap/fonts"
+css_dir = "public/gmap/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
